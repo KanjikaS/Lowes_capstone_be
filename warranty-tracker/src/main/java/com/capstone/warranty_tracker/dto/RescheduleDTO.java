e01e44882aa1f3597490fb92f8c2bef4023fc6ef
Apoorva Singh <108567500+apoorvasinngh@users.noreply.github.com>
2025-06-25 17:21:58 +0530

    Revert "Appointment Management BE added"

--- warranty-tracker/src/main/java/com/capstone/warranty_tracker/dto/RescheduleDTO.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.warranty_tracker.dto;
-
-import java.time.LocalDateTime;
-
-public record RescheduleDTO(
-        LocalDateTime scheduledSlot
-) {}
-
--- warranty-tracker/src/main/java/com/capstone/warranty_tracker/dto/ServiceRequestDto.java
+++ warranty-tracker/src/main/java/com/capstone/warranty_tracker/dto/ServiceRequestDto.java
@@ -9,6 +9,5 @@ public class ServiceRequestDto {
     private String serialNumber;
     private String issueDescription;
     private LocalDateTime preferredSlot;
-    private Long technicianId;
 }
 
